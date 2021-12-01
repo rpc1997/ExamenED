e67769c53a99607d97a5e23a8bd1cf7e64c40dc5
rubén <>
2021-12-01 17:28:04 +0100

    Reto 2 realizado

---
+++ ExamenED/src/examened/Reto2.java
@@ -0,0 +1,12 @@
+package examened;
+
+
+public class Reto2 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1000; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
